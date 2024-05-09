bb1ace89fa77d05a6865601ee7fe618efe943eb0
dnwls3624 <166504773+dnwls3624@users.noreply.github.com>
2024-05-09 21:49:26 +0900

    Add files via upload

---
+++ Day11/dataType.py
@@ -0,0 +1,20 @@
+# data type
+# 데이터 타입은 메모리에 차지 하는 크기를 알려주는 것
+# 기본 타입: str,int,float,bool
+# 참조 타입: list,dict,set,Dog, ...
+
+# 동적 타입 프로그래밍 언어: python, javascript
+# 정적 타입 프로그래밍 언어: java, c, kotlin, ...
+
+# 한줄 요약: 자유도와 책임도 차이를 가진다.
+# 어떤 언어가 좋나요?
+# 학문적으로: c언어 -> ...
+# 비지니스로:
+# 웹: javascript, typescript
+# 안드로이드 어플: java, kotlin, typescript
+# IOS 어플: swift
+# 하이브리드 플랫폼 언어 어플: 플러터
+# Iot: C언어
+# 인공지능 미래: python
+
+
